180025fffa1fc30617c9b7c12e87cf8db1e1f2e3
Moses Ogwo <mosesogwo@gmail.com>
2019-07-17 14:28:03 +0100

    finish post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, 
               uniqueness: true, length: { maximum: 10 }
   validates :email, presence: true
+
+  has_many :posts
+  # has_many :comments
 end
